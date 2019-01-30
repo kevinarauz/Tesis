544c490149bf662721fd94f03b6981b2ace2d9a6
kevinarauz <kecin222294@gmail.com>
2019-01-29 22:16:05 -0500

    Pagina Web

--- PaginaInicio.py
+++ PaginaInicio.py
@@ -1,24 +1,40 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
 
+# Inicialización
 app = Flask(__name__)
-Bootstrap(app)
+
+# Mysql Connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'tesis'
+mysql = MySQL(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/principal')
 def principal():
     return render_template('index.html')
 
+
 @app.route('/tablas')
 def tablas():
-    return render_template('tablas.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM encuestas')
+    datos = cur.fetchall()
+    cur.close()
+    return render_template('tablas.html', encuestas = datos)
+
 
 @app.route('/graficos')
 def graficos():
     return render_template('graficos.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
