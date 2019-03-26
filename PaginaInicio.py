5768294aeb0164522092555b5c4d8bd970cdc835
kevinarauz <kecin222294@gmail.com>
2019-03-26 18:25:43 -0500

    Pagina Web

--- PaginaInicio.py
+++ PaginaInicio.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request
 from flask_mysqldb import MySQL
 
 # Inicialización
@@ -11,42 +11,76 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'tesis'
 mysql = MySQL(app)
 
-#Ejemplo de uso python anyware
+#Uso en python anyware
 #app.config['MYSQL_HOST'] = 'keviin.mysql.pythonanywhere-services.com'
 #app.config['MYSQL_USER'] = 'keviin'
 #app.config['MYSQL_PASSWORD'] = 'naruto29'
 #app.config['MYSQL_DB'] = 'keviin$tesis'
+#mysql = MySQL(app)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def default():
+    return render_template('inicio.html')
 
+@app.route('/Inicio')
+def inicio():
+    return render_template('inicio.html')
 
-@app.route('/principal')
-def principal():
-    return render_template('index.html')
+@app.route('/Introducción')
+def Introduccion():
+    return render_template('Introducción.html')
 
+@app.route('/Problemática')
+def Problematica():
+    return render_template('Problemática.html')
 
-@app.route('/tablas')
-def tablas():
+@app.route('/MarcoLógico')
+def MarcoLogico():
+    return render_template('MarcoLógico.html')
+
+@app.route('/TipoDeInvestigación')
+def TipoDeInvestigacion():
+    return render_template('TipoDeInvestigación.html')
+
+@app.route('/PoblaciónMuestra')
+def PoblacionMuestra():
+    return render_template('PoblaciónMuestra.html')
+
+@app.route('/GráficosEstadísticos')
+def GraficosEstadisticos():
+    return render_template('GráficosEstadísticos.html')
+
+@app.route('/TablaDeEncuestas')
+def TablaDeEncuestas():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM encuestas')
     datos = cur.fetchall()
     cur.close()
-    return render_template('tablas.html', encuestas = datos)
+    return render_template('TablaDeEncuestas.html', encuestas=datos)
 
+@app.route('/Metodología')
+def Metodologia():
+    return render_template('Metodología.html')
 
-@app.route('/graficos')
-def graficos():
-    return render_template('graficos.html')
+@app.route('/TablaDeVariables')
+def TablaDeVariables():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM factores')
+    datos = cur.fetchall()
+    cur.close()
+    return render_template('TablaDeVariables.html', factores=datos)
 
-@app.route('/DatosVariables')
-def DatosVariables():
-    return render_template('DatosVariables.html')
+@app.route('/Resultados')
+def Resultados():
+    return render_template('Resultados.html')
 
-@app.route('/EncuestaExpertos')
-def EncuestaExpertos():
-    return render_template('EncuestaExpertos.html')
+@app.route('/Bibliografía')
+def Bibliografia():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM bibliografias')
+    datos = cur.fetchall()
+    cur.close()
+    return render_template('Bibliografía.html', bibliografias=datos)
 
 if __name__ == '__main__':
     app.run(debug=True)
